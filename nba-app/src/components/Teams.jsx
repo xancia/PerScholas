5d7e84acb2baf2305576c5953bbb4736d4db3ffb
Xancia <cminhma@gmail.com>
2023-12-15 10:09:14 -0500

    moved folders

--- nba-app/src/components/Teams.jsx
+++
@@ -1,8 +0,0 @@
-
-const Teams = () => {
-  return (
-    <div>Teams</div>
-  )
-}
-
-export default Teams
--- nba-app/src/components/store/store.jsx
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import teamDataReducer from "./teamDataSlice";
-
-export const store = configureStore({
-    reducer: {
-        teamData: teamDataReducer,
-    }
-})
