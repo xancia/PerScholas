3206d73d73d92ab56eb9d5289805df079ed19a2b
Xancia <cminhma@gmail.com>
2024-01-12 09:36:46 -0500

    set up models and a few routes

---
+++ FOOD-MVC/models/fruits.js
@@ -0,0 +1,34 @@
+const fruits = [
+    {
+        name:'apple',
+        color: 'red',
+        readyToEat: true
+    },
+    {
+        name:'pear',
+        color: 'green',
+        readyToEat: false
+    },
+    {
+        name:'banana',
+        color: 'yellow',
+        readyToEat: true
+    },
+    {
+        name:'kiwi',
+        color:'green',
+        readyToEat: true
+    },
+    {
+        name: 'plum',
+        color: 'purple',
+        readyToEat: true
+    },
+    {
+        name: 'mango',
+        color: 'greenish orange',
+        readyToEat: false
+    }
+];
+
+module.exports = fruits
--- FOOD-MVC/server.js
+++ FOOD-MVC/server.js
@@ -2,8 +2,25 @@ const express = require('express')
 
 const app = express()
 
+const fruits = require('./models/fruits')
+
 const PORT = 5000
 
+// root route
 app.get('/', (req,res) => {
     res.send('Hello World!')
+})
+
+// index route
+app.get('/fruits', (req,res) => {
+    res.send(fruits)
+})
+
+//show route
+app.get('/fruits/:index', (req,res) => {
+    res.send(fruits[req.params.index])
+})
+
+app.listen(PORT, () => {
+    console.log('Listening on port: ' + PORT)
 })
