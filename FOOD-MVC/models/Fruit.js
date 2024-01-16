52602e1561578ab031ed551eea6dd17d63da063c
Xancia <cminhma@gmail.com>
2024-01-16 11:38:53 -0500

    update

--- FOOD-MVC/models/Fruit.js
+++ FOOD-MVC/models/Fruit.js
@@ -3,11 +3,12 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const fruitSchema = new Schema({
-    name:{type:String},
+    name:{type:String,required:true},
     color:{type:String},
     readyToEat:{type:Boolean}
 })
 
+// fruits is the name of collection, fruitschema is 
 const Fruit = mongoose.model('fruits', fruitSchema)
 
 module.exports = Fruit
