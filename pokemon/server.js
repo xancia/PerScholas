3fa419dd63e644a5cdb9aff9a5b8a72c974e9b9e
Xancia <cminhma@gmail.com>
2024-01-23 02:27:34 -0500

    removed undeed section

--- pokemon/server.js
+++ pokemon/server.js
@@ -1,23 +1,15 @@
 
 const express = require("express");
 const jsxEngine = require("jsx-view-engine");
-
-
-const pokemon = require("./models/pokemon"); 
-
-
-const pokemonRoutes = require("./routes/pokemonRoutes");
-
-
 const app = express();
 const PORT = 3000;
 
+const pokemonRoutes = require("./routes/pokemonRoutes");
 
 app.set("view engine", "jsx");
 
 app.engine("jsx", jsxEngine());
 app.use("/pokemon", pokemonRoutes); 
-
 
 
 app.get("/", (req, res) => {
