8a00b1db2d23f233263d47ae7aae08df815a3628
Xancia <cminhma@gmail.com>
2024-01-23 10:22:22 -0500

    set up routing

--- social-clone/client/src/App.jsx
+++ social-clone/client/src/App.jsx
@@ -1,11 +1,16 @@
-
+import { Routes , Route } from "react-router-dom"
 
 function App() {
   
 
   return (
     <>
-      test
+      <Routes>
+        <Route path="/" element={null} />
+        <Route path="/register" element={null} />
+        <Route path="/login" element={null} />
+        <Route path="/profile" element={null} />
+      </Routes>
     </>
   )
 }
